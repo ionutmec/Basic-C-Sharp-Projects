dbc3ce95fc05e48f4d7c98ac9cbe6c2f2937a6ce
ionutmec <ionutmecu@gmail.com>
2024-10-15 09:45:02 +0100

    Add project files.

---
+++ WebApp Tutorial/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//functions to be called onemouseover and onemouseout to change the color of "Hello World" 
+function newColor() {
+    document.getElementById("hello_world").style.color = "red";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos ==50) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
